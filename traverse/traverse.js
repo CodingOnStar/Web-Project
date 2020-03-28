4009b29afb333b930f7e1d4f6d17aa420d3f7c07
CodingOnStar <447357187@qq.com>
2020-03-28 23:32:11 +0800

    with different color in active status

--- traverse/traverse.js
+++ traverse/traverse.js
@@ -2,7 +2,7 @@
  * @Author: Hanxu Jiang 
  * @Date: 2020-03-27 20:52:32 
  * @Last Modified by: Hanxu Jiang
- * @Last Modified time: 2020-03-28 23:08:43
+ * @Last Modified time: 2020-03-28 23:30:58
  */
 var timer = null;
 var animateList = [];
@@ -11,7 +11,30 @@ var preOrder = document.querySelector('#preOrder');
 var inOrder = document.querySelector('#inOrder');
 var postOrder = document.querySelector('#postOrder');
 
+// function to generate random number
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+}
+//随机选取一个颜色
+function randomColor() {
+    return 'rgb(' +
+        random(0, 255) + ', ' +
+        random(0, 255) + ', ' +
+        random(0, 255) + ')';
+}
+/*
+function getRandomColor() {
 
+    var divs = document.getElementsByTagName("div");
+
+    for (i = 0; i < divs.length; i++) {
+
+        divs[i].style.backgroundColor = randomColor();
+
+    }
+}
+*/
 //add Event
 //IE7和8不支持addEventListener和removeEventListener，取而代之的是attachEvent和detachEvent
 function addClick(element, type, handler) {
@@ -64,22 +87,27 @@ var orderItems = {
     reset() {
         animateList.forEach(function (item) {
             item.className = item.className.replace('active', '');
+            item.style.backgroundColor = '';
         });
     },
     animate() {
         var i = 0;
         animateList[i].className += ' active';
+        animateList[i].style.backgroundColor = randomColor();
         timer = setInterval(function () {
             //这里不能用箭头函数，想一想为什么
             i++;
             if (i < animateList.length) {
                 //前一个结点回复平常，当前结点active
                 animateList[i - 1].className = animateList[i - 1].className.replace('active', '');
+                animateList[i - 1].style.backgroundColor = '';
                 animateList[i].className += 'active';
+                animateList[i].style.backgroundColor = randomColor();
             }
             else {
                 clearInterval(timer);
                 animateList[i - 1].className = animateList[i - 1].className.replace('active', '');
+                animateList[i - 1].style.backgroundColor = '';
             }
         }, 500);
     }
@@ -116,6 +144,7 @@ var operation = {
         this.addButtonEvent();
     }
 };
+
 operation.init();
 
 
