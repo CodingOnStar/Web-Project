8e0f4110c4076f1e41422b8ab7e883f4d2d27f0a
Hanxu Jiang-Linux <447357187@qq.com>
2020-09-15 14:58:58 +0800

    加入了缓存

--- exercise/Fibonacci.js
+++ exercise/Fibonacci.js
@@ -1,7 +1,25 @@
 /*https://ruanyifeng.com/blog/2015/04/tail-call.html
 关于尾调用优化*/
 
-function fibonacciMemoization(n) {
+//使用函数式变成进行缓存结果的fibonacci实现
+function memoize (fn) {
+    const cache = {};
+    return function () {
+        const key = JSON.stringify(arguments);
+        var value = cache[key];
+        if (!value) {
+            value = [fn.apply(null, arguments)];  // 放在一个数组中，方便应对 undefined，null 等异常情况
+            cache[key] = value;
+        }
+        return value[0];
+    }
+}
+
+const fibonacci = memoize(n => n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2));
+console.log(fibonacci(4))  // 执行后缓存了 fibonacci(2), fibonacci(3),  fibonacci(4)
+console.log(fibonacci(10)) // fibonacci(2), fibonacci(3),  fibonacci(4) 的结果直接从缓存中取出，同时缓存其他的
+
+function fibonacciMemoization (n) {
     const memo = [0, 1]
     const fibonacci = (n) => {
         if (memo[n] != null) return memo[n]
